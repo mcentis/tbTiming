c0a496da1fe60de867ec4e5bbd72225a3b5ab197
mcentis <matteo.centis.vignali@cern.ch>
2018-11-15 16:52:43 +0100

    implemented hitmaps and amplitude in x and y slices plots

--- include/AnalyzeWithTracking.hh
+++ include/AnalyzeWithTracking.hh
@@ -4,6 +4,8 @@
 #include "ConfigFileReader.hh"
 #include "ScopeTreeInterface.hh"
 
+#include "TH2F.h"
+
 class AnalyzeWithTracking{
 public:
   static const int _nCh = ScopeTreeInterface::_inCh; // use the number of channels from the interface class
@@ -22,7 +24,13 @@ private:
   TFile* _outFile;
 
   void AssociateBranches();
-
+  void InitializePlots();
+  template<typename T> void ReadCfgArray(T* parameter, const char* key); // duplicate... check if possible to change
+  //void ReadTimingPairs();
+  void ReadCfg();
+  
+  // ============================= variables for the trees ==================
+  
   ULong64_t _event; // event number, needed by the tree
   UInt_t _nChTree; // number of channels as needed by the tree (cannot use _nCh directly)
 
@@ -37,9 +45,26 @@ private:
   Float_t* _tCFD; // time when the CFD threshold from cfg file is crossed
 
   Int_t _nTracks; // number of tracks in the event, it is 0 if no hit is reconstructed
-  Float_t*** _hits; // hit position on the DUT planes (one for each oscilloscope channel)
+  Float_t _hits[500][_nCh][3]; // hit position on the DUT planes (one for each oscilloscope channel)
   Double_t* _pars; // parameters of the tracks
 
+  // ============================= variables from conf file =====================
+
+  float* _thr; // array with signal thresholds in V to select events in the analysis
+  float* _maxAmpliCut; // array with maximum amplitude cut in V to select events in the analysis, to avoid saturation
+  float* _xSliceLow; // array with lower limit of the cut in x
+  float* _xSliceHigh; // array with higher limit of the cut in x
+  float* _ySliceLow; // array with lower limit of the cut in y
+  float* _ySliceHigh; // array with higher limit of the cut in y
+
+  //std::vector<std::string> _timingPairs; // pairs of channels for the timing
+
+  // ============================= plots ========================================
+  
+  TH2F** _hitMaps; // hitmaps on each plane
+  TH2F** _hitMapsWithThr; // hitmaps on each plane with threshold
+  TH2F** _ampliVsX; // amplitude as a function of the position in X, in slice selected by the cut in y
+  TH2F** _ampliVsY; // amplitude as a function of the position in Y, in slice selected by the cut in x
 };
 
 #endif //#ifndef ANALYZEWITHTRACKING_HH
--- src/AnalyzeWithTracking.cc
+++ src/AnalyzeWithTracking.cc
@@ -16,6 +16,8 @@ AnalyzeWithTracking::AnalyzeWithTracking(const char* scopeAnaName, const char* t
   _pulsePropTree->AddFriend(_hitTree);
 
   AssociateBranches();
+
+  ReadCfg();
   
   // open output root file
   std::string outFileName = scopeAnaName;
@@ -30,6 +32,8 @@ AnalyzeWithTracking::AnalyzeWithTracking(const char* scopeAnaName, const char* t
   
   _outFile = TFile::Open(outFileName.c_str(), "RECREATE");
 
+  InitializePlots();
+  
   return;
 }
 
@@ -44,8 +48,10 @@ AnalyzeWithTracking::~AnalyzeWithTracking(){
   delete[] _linRegT0;
   delete[] _integral;
   delete[] _tCFD;
-  delete[] _hits;
   delete[] _pars;
+
+  delete[] _thr;
+  delete[] _maxAmpliCut;
   
   return;
 }
@@ -62,13 +68,6 @@ void AnalyzeWithTracking::AssociateBranches(){
   _tCFD = new Float_t[_nCh];
 
   _nTracks = 500;
-  _hits = new Float_t**[_nTracks];
-  for(int i = 0; i < _nTracks; ++i){
-    _hits[i] = new Float_t*[_nCh];
-      for(int j = 0; j < _nCh; ++j)
-	_hits[i][j] = new Float_t[3];
-  }
-
   _pars = new Double_t[4];
   
   _pulsePropTree->SetBranchAddress("event", &_event);
@@ -98,12 +97,139 @@ void AnalyzeWithTracking::Analyze(){
     if((i+1) % 1000 == 0 || (i+1) == nEntries)
       std::cout << " Processing event " << i+1 << " / " << nEntries << "                             \r" << std::flush;
     
+    if(_nTracks != 1) // use only events with one track
+      continue;
+    
+    for(int iCh = 0; iCh < _nCh; ++iCh){ // hitmaps
+      _hitMaps[iCh]->Fill(_hits[_nTracks-1][iCh][0], _hits[_nTracks-1][iCh][1]);
+
+      if(_ampli[iCh] >= _thr[iCh])
+      	_hitMapsWithThr[iCh]->Fill(_hits[_nTracks-1][iCh][0], _hits[_nTracks-1][iCh][1]);
+    }
+
+    for(int iCh = 0; iCh < _nCh; ++iCh){
+      if(_hits[_nTracks-1][iCh][1] >= _ySliceLow[iCh] && _hits[_nTracks-1][iCh][1] <= _ySliceHigh[iCh])
+	_ampliVsX[iCh]->Fill(_hits[_nTracks-1][iCh][0], _ampli[iCh]);
+
+      if(_hits[_nTracks-1][iCh][0] >= _xSliceLow[iCh] && _hits[_nTracks-1][iCh][0] <= _xSliceHigh[iCh])
+	_ampliVsY[iCh]->Fill(_hits[_nTracks-1][iCh][1], _ampli[iCh]);
+    }
+    
+  }
+
+  std::cout << std::endl;
+  
+  return;
+}
+
+void AnalyzeWithTracking::InitializePlots(){
+  char name [50];
+  char title[200];
+
+  _hitMaps = new TH2F*[_nCh];
+  for(int iCh = 0; iCh < _nCh; ++iCh){
+    sprintf(name, "hitMap_Ch%d", iCh+1);
+    sprintf(title, "Hit Map Ch%d;X [mm];Y [mm];Entries", iCh+1);
+    _hitMaps[iCh] = new TH2F(name, title, 500, 0, 100, 500, -50, 50);
+  }
+
+  _hitMapsWithThr = new TH2F*[_nCh];
+  for(int iCh = 0; iCh < _nCh; ++iCh){
+    sprintf(name, "hitMapWithThr_Ch%d", iCh+1);
+    sprintf(title, "Hit Map Ch%d threshold %d mV;X [mm];Y [mm];Entries", iCh+1, (int) (_thr[iCh] * 1000));
+    _hitMapsWithThr[iCh] = new TH2F(name, title, 500, 0, 100, 500, -50, 50);
+  }
+
+  _ampliVsX = new TH2F*[_nCh];
+  for(int iCh = 0; iCh < _nCh; ++iCh){
+    sprintf(name, "ampliVsX_Ch%d", iCh+1);
+    sprintf(title, "Amplitude Ch%d;X [mm];Amplitude [V];Entries", iCh+1);
+    _ampliVsX[iCh] = new TH2F(name, title, 500, 0, 100, 200, 0, 1);
+  }
+
+  _ampliVsY = new TH2F*[_nCh];
+  for(int iCh = 0; iCh < _nCh; ++iCh){
+    sprintf(name, "ampliVsY_Ch%d", iCh+1);
+    sprintf(title, "Amplitude Ch%d;Y [mm];Amplitude [V];Entries", iCh+1);
+    _ampliVsY[iCh] = new TH2F(name, title, 500, -50, 50, 200, 0, 1);
+  }
+
+  return;
+}
+
+void AnalyzeWithTracking::Save(){
+  _outFile->cd();
+
+  TDirectory* dir = _outFile->mkdir("hitMaps");
+  dir->cd();
+  for(int iCh = 0; iCh < _nCh; ++iCh)
+    _hitMaps[iCh]->Write();
+
+  dir = _outFile->mkdir("hitMapsWithThr");
+  dir->cd();
+  for(int iCh = 0; iCh < _nCh; ++iCh)
+    _hitMapsWithThr[iCh]->Write();
+
+  dir = _outFile->mkdir("amplitudeSlices");
+  dir->cd();
+  for(int iCh = 0; iCh < _nCh; ++iCh){
+    _ampliVsX[iCh]->Write();
+    _ampliVsY[iCh]->Write();
   }
   
   return;
 }
 
-void AnalyzeWithTracking::Save(){
+void AnalyzeWithTracking::ReadCfg(){
+  _thr = new float[_nCh];
+  _maxAmpliCut = new float[_nCh];
+
+  _xSliceLow = new float[_nCh];
+  _xSliceHigh = new float[_nCh];
+  _ySliceLow = new float[_nCh];
+  _ySliceHigh = new float[_nCh];
+  
+  ReadCfgArray(_thr, "threshold");
+  ReadCfgArray(_maxAmpliCut, "maxAmpli");
+
+  ReadCfgArray(_xSliceLow, "xSliceLow");
+  ReadCfgArray(_xSliceHigh, "xSliceHigh");
+  ReadCfgArray(_ySliceLow, "ySliceLow");
+  ReadCfgArray(_ySliceHigh, "ySliceHigh");
+  
+  return;
+}
+
+// void AnalyzeWithTracking::ReadTimingPairs(){ // duplicate... check if possible to change
+//   std::string valStr = _cfg->GetValue("timingPairs");
+//   std::stringstream strstr(valStr);
+//   std::string sub;
+  
+//   while(strstr.good()){
+//     getline(strstr, sub, ',');
+//     _timingPairs.push_back(sub);
+//   }
+
+//   //for(std::vector<std::string>::iterator it = _timingPairs.begin(); it != _timingPairs.end(); ++it)
+//   //  std::cout << *it << std::endl;
+  
+//   return;
+// }
+
+template<typename T> void AnalyzeWithTracking::ReadCfgArray(T* parameter, const char* key){ // duplicate... check if possible to change
+  std::string valStr = _cfg->GetValue(key);
+  std::stringstream strstr(valStr);
+  std::string sub;
+  
+  for(int i = 0; i < _nCh; ++i){
+    if(strstr.good() == false){
+      std::cout << "Error while reading config file for parameter " << key << std::endl;
+      exit(1);
+    }
+    getline(strstr, sub, ',');
+    parameter[i] = atof(sub.c_str());
+  }
+  
   return;
 }
 
